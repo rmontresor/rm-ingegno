3034473b213497048e66b6c143f796a17b5f063f
rmontresor <riccardo.montresor@csgalileo.org>
2023-09-19 09:14:32 +0200

    fix app module

--- frontend/web-ingegno/src/app/app.module.ts
+++ frontend/web-ingegno/src/app/app.module.ts
@@ -5,17 +5,68 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { TestPageComponent } from "./pages/test-page/test-page.component";
 import { CubeComponent } from "./components/cube/cube.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { RoomComponent } from "./components/room/room.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { RmBioComponent } from "./pages/rm-bio/rm-bio.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { CanvasBoxComponent } from "./components/logo-box/logo-box.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MdbAccordionModule } from "mdb-angular-ui-kit/accordion";
+import { MdbCarouselModule } from "mdb-angular-ui-kit/carousel";
+import { MdbCheckboxModule } from "mdb-angular-ui-kit/checkbox";
+import { MdbCollapseModule } from "mdb-angular-ui-kit/collapse";
+import { MdbDropdownModule } from "mdb-angular-ui-kit/dropdown";
+import { MdbFormsModule } from "mdb-angular-ui-kit/forms";
+import { MdbModalModule } from "mdb-angular-ui-kit/modal";
+import { MdbPopoverModule } from "mdb-angular-ui-kit/popover";
+import { MdbRadioModule } from "mdb-angular-ui-kit/radio";
+import { MdbRangeModule } from "mdb-angular-ui-kit/range";
+import { MdbRippleModule } from "mdb-angular-ui-kit/ripple";
+import { MdbScrollspyModule } from "mdb-angular-ui-kit/scrollspy";
+import { MdbTabsModule } from "mdb-angular-ui-kit/tabs";
+import { MdbTooltipModule } from "mdb-angular-ui-kit/tooltip";
+import { MdbValidationModule } from "mdb-angular-ui-kit/validation";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TeamComponent } from "./pages/team/team.component";
+import { ProfileComponent } from "./components/profile/profile.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent,
-    TestPageComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CanvasBoxComponent,
+    RoomComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeamComponent,
+    ProfileComponent,
     RmBioComponent,
+    CubeComponent,
+    TestPageComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
